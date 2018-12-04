45ce5bc13336215fae6de771ee719f5d9e31a9e2
Ryo Nitami <nitry.n2t1m1+github@gmail.com>
2018-12-04 10:07:50 +0900

    Add MainAdapter

--- app/src/main/java/android/rnita/me/frontpage/MainActivity.kt
+++ app/src/main/java/android/rnita/me/frontpage/MainActivity.kt
@@ -1,12 +1,17 @@
 package android.rnita.me.frontpage
 
 import android.os.Bundle
+import android.rnita.me.frontpage.databinding.MainActivityBinding
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+
+        val binding = DataBindingUtil.setContentView<MainActivityBinding>(this, R.layout.main_activity)
+        val adapter = MainAdapter()
+        
     }
 }
