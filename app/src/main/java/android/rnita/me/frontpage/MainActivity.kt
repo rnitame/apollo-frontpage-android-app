d018fcac32556486095bcad58085ce85e50a536f
Ryo Nitami <nitry.n2t1m1+github@gmail.com>
2018-12-07 20:44:26 +0900

    Add apollo libraries

--- app/src/main/java/android/rnita/me/frontpage/MainActivity.kt
+++ app/src/main/java/android/rnita/me/frontpage/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.rnita.me.frontpage.databinding.MainActivityBinding
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +13,8 @@ class MainActivity : AppCompatActivity() {
 
         val binding = DataBindingUtil.setContentView<MainActivityBinding>(this, R.layout.main_activity)
         val adapter = MainAdapter()
-        
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = adapter
     }
 }
