832205db9b87a02f032275b9112868c77a984618
Ryo Nitami <nitry.n2t1m1+github@gmail.com>
2018-12-07 20:45:43 +0900

    Add libraries and Adapter class (#1)
    * Add MainAdapter
    
    * Add apollo libraries

--- app/src/main/java/android/rnita/me/frontpage/MainActivity.kt
+++ app/src/main/java/android/rnita/me/frontpage/MainActivity.kt
@@ -1,12 +1,20 @@
 package android.rnita.me.frontpage
 
 import android.os.Bundle
+import android.rnita.me.frontpage.databinding.MainActivityBinding
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+
+        val binding = DataBindingUtil.setContentView<MainActivityBinding>(this, R.layout.main_activity)
+        val adapter = MainAdapter()
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = adapter
     }
 }
--- app/src/main/java/android/rnita/me/frontpage/MainAdapter.kt
+++ app/src/main/java/android/rnita/me/frontpage/MainAdapter.kt
@@ -1,4 +1,23 @@
 package android.rnita.me.frontpage
 
-class MainAdapter {
+import android.rnita.me.frontpage.databinding.PostContentBinding
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MainAdapter : RecyclerView.Adapter<MainAdapter.PostContentViewHolder>() {
+
+    val posts = mutableListOf<String>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostContentViewHolder =
+        PostContentViewHolder(PostContentBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+    override fun getItemCount(): Int = posts.size
+
+    override fun onBindViewHolder(holder: PostContentViewHolder, position: Int) {
+        val post = posts[position]
+        holder.binding.title.text = post
+    }
+
+    inner class PostContentViewHolder(val binding: PostContentBinding) : RecyclerView.ViewHolder(binding.root)
 }
