63a273699df9e8b7be7347c05a0c0083a185a84d
Ryo Nitami <nitry.n2t1m1+github@gmail.com>
2018-12-13 22:44:27 +0900

    Apply values on server

--- app/src/main/java/android/rnita/me/frontpage/MainAdapter.kt
+++ app/src/main/java/android/rnita/me/frontpage/MainAdapter.kt
@@ -7,7 +7,10 @@ import androidx.recyclerview.widget.RecyclerView
 
 class MainAdapter : RecyclerView.Adapter<MainAdapter.PostContentViewHolder>() {
 
-    val posts = mutableListOf<String>()
+    val posts = mutableListOf<PostsQuery.Post>()
+
+    var onItemClicked: ((Int) -> Unit)? = null
+    var onButtonClicked: ((Int) -> Unit)? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostContentViewHolder =
         PostContentViewHolder(PostContentBinding.inflate(LayoutInflater.from(parent.context), parent, false))
@@ -15,8 +18,17 @@ class MainAdapter : RecyclerView.Adapter<MainAdapter.PostContentViewHolder>() {
     override fun getItemCount(): Int = posts.size
 
     override fun onBindViewHolder(holder: PostContentViewHolder, position: Int) {
-        val post = posts[position]
-        holder.binding.title.text = post
+        val post = posts[position].fragments().postFragment()
+        holder.binding.title.text = post.title()
+        holder.binding.name.text = "by ${post.author()?.firstName()} ${post.author()?.lastName()}"
+        holder.binding.vote.text = "${post.votes()} votes"
+
+        holder.binding.button.setOnClickListener {
+            onButtonClicked?.invoke(post.id())
+        }
+        holder.binding.root.setOnClickListener {
+            onItemClicked?.invoke(post.author()?.id() ?: return@setOnClickListener)
+        }
     }
 
     inner class PostContentViewHolder(val binding: PostContentBinding) : RecyclerView.ViewHolder(binding.root)
